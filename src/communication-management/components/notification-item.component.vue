e4cfc39cf0c6e878593c8e35c6c8ec6bd9e693d5
Eric <erich56000@gmail.com>
2024-09-24 16:41:38 -0500

    feat(notifications): add methods and configure the notification-item component

--- src/communication-management/components/notification-item.component.vue
+++ src/communication-management/components/notification-item.component.vue
@@ -1,9 +1,25 @@
 <script setup>
-
+const props = defineProps({
+  notification: Object
+});
 </script>
 
 <template>
+  <div style="width: 100%; height:100%">
+    <pv-card>
+      <template #title>{{ notification.date }}</template>
+      <template #content>
 
+        <div class="flex gap-4 mt-1">
+          <span class="pi pi-bell" style="scale: 2.5"></span>
+          <p class="m-0"> {{notification.content}} </p>
+          <div style="margin-left: auto">
+            <a :href="notification.endpoint">Go to the section</a>
+          </div>
+        </div>
+      </template>
+    </pv-card>
+  </div>
 </template>
 
 <style scoped>
