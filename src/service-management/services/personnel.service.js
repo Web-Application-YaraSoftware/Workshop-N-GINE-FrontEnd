99b9b9f196caf75bc238c6ab4572857ac3b0d350
Elmer Riva <elmeraugustoriva@gmail.com>
2024-09-24 19:19:42 -0500

    refactor(personnel.service): update crud methods to include workshop and user type query parameters

--- src/service-management/services/personnel.service.js
+++ src/service-management/services/personnel.service.js
@@ -2,18 +2,18 @@ import http from "../../shared/services/http-common.js";
 
 export class PersonnelService {
     getAllPersonnel() {
-        return http.get('/users');
+        return http.get('/users?workshop_id=1&user_type.id=2');
     }
 
     postPersonnel(data) {
-        return http.post('/users', data);
+        return http.post('/users?workshop_id=1&user_type.id=2', data);
     }
 
     deletePersonnel(id) {
-        return http.delete(`/users/${id}`);
+        return http.delete(`/users/${id}?workshop_id=1&user_type.id=2`);
     }
 
     putPersonnel(id, data) {
-        return http.put(`/users/${id}`, data);
+        return http.put(`/users/${id}?workshop_id=1&user_type.id=2`, data);
     }
 }
