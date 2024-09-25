e2acc012ded1fe3d8376fce365270129953460a2
Eric <erich56000@gmail.com>
2024-09-25 02:16:59 -0500

    feat(inventory): add header component for inventory pages

--- src/service-management/components/inventory-header.component.vue
+++ src/service-management/components/inventory-header.component.vue
@@ -1,10 +1,35 @@
 <script setup>
 
+import {ref} from "vue";
+
+let title=ref("Stock");
+
+function titleHeader(newTitle){
+  title.value = newTitle;
+  console.log(title)
+}
+
 </script>
 
 <template>
-  <h1>Stock- Requests</h1>
-  <router-view/>
+  <section>
+    <pv-card>
+      <template #title>{{title}}</template>
+      <template #content class="w-full">
+        <router-link to="/inventory/stock">
+          <pv-button label="In Stock" class="mr-4 mt-6" @click="titleHeader('Stock')"/>
+        </router-link>
+
+        <router-link to="/inventory/requests">
+          <pv-button label="Requests" @click="titleHeader('Requests')"/>
+        </router-link>
+      </template>
+    </pv-card>
+    <router-view/>
+  </section>
+
+
+
 </template>
 
 <style scoped>
