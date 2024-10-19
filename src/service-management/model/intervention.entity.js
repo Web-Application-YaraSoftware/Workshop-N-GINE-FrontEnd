dccd1bce6555b626c3818154b4d3bb232cdd7e27
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-19 01:03:50 -0500

    refactor(intervention): update Intervention class structure and add finish method
    - Refactored the `Intervention` class to use object destructuring in the constructor with default values.
    - Added `vehicle` and `interventionType` properties.
    - Implemented `finish` method to mark intervention as completed and set completion date.

--- src/service-management/model/intervention.entity.js
+++ src/service-management/model/intervention.entity.js
@@ -1,13 +1,32 @@
+import {Vehicle} from "./vehicle.entity.js";
+import {InterventionType} from "./intervention-type.enum.js";
+import {InterventionState} from "./intervention-state.enum.js";
+
 export class Intervention {
-    constructor(id, workshopId, mechanicLeaderId, vehicleId, clientId, registrationDate, completionDate, state) {
+    constructor({
+                    id = 0,
+                    workshopId = 0,
+                    vehicle = new Vehicle(),
+                    mechanicLeaderId = 0,
+                    registrationDate = '',
+                    completionDate = '',
+                    interventionType = InterventionType.REPARATION,
+                    state = InterventionState.PENDING,
+                    description = ''
+                } = {}) {
         this.id = id;
         this.workshopId = workshopId;
+        this.vehicle = vehicle;
         this.mechanicLeaderId = mechanicLeaderId;
-        this.vehicleId = vehicleId;
-        this.clientId = clientId;
-        this.registrationDate = registrationDate;
-        this.completionDate = completionDate;
+        this.registrationDate = registrationDate ? new Date(registrationDate) : new Date();
+        this.completionDate = completionDate ? new Date(completionDate) : new Date();
+        this.interventionType = interventionType;
         this.state = state;
+        this.description = description;
     }
 
+    finish() {
+        this.state = InterventionState.COMPLETED;
+        this.completionDate = new Date();
+    }
 }
