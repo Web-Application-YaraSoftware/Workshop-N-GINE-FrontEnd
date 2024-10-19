ae061006a54e36f39f5dbebc97c8c4d6671304f5
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-10-19 01:08:10 -0500

    feat(workshop-store): add mechanicType state with leader and assistant setters
    - Renamed `categoryService` to `workshopService` for clarity.
    - Added `mechanicType` state with options for Leader and Assistant.
    - Implemented `setLeader` and `setAssistant` methods to update the `mechanicType`.

--- src/shared/services/workshop-store.js
+++ src/shared/services/workshop-store.js
@@ -4,25 +4,45 @@ import {WorkshopService} from "../../service-management/services/workshop.servic
 import {Workshop} from "../model/workshop.entity.js";
 
 export const useWorkshopStore = defineStore( 'workshop', ()=>{
-    const categoryService = new WorkshopService();
+    const workshopService = new WorkshopService();
 
     const workshop = ref();
     const role = ref(1);
     const user = ref({
         id: 1
-    })
+    });
+    const mechanicType = ref(null);
+    const mechanicTypes = [
+        {
+            id: 1,
+            name: 'Leader'
+        },
+        {
+            id: 2,
+            name: 'Assistant'
+        }
+    ];
 
     function getWorkshop(id = 1){
-        categoryService.getById(id)
+        workshopService.getById(id)
             .then(response => {
                 workshop.value = new Workshop(response.data);
             });
+    }
+    function setLeader(){
+        mechanicType.value = mechanicTypes[0];
+    }
+    function setAssistant(){
+        mechanicType.value = mechanicTypes[1];
     }
 
     return {
         workshop,
         role,
         user,
-        getWorkshop
+        mechanicType,
+        getWorkshop,
+        setLeader,
+        setAssistant
     }
 })
