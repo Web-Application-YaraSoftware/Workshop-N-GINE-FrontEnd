97dba59f6df40c67641bd464aa799e434cfdd722
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-19 00:40:29 -0500

    feat(vehicle-service): add vehicle service with methods to retrieve and create vehicles

---
+++ src/cmr/services/vehicle.service.js
@@ -0,0 +1,24 @@
+import http from "../../shared/services/http-common.js";
+
+export class VehicleService {
+    resourceEndpoint = '/vehicles';
+
+    getByClientId(clientId) {
+        return http.get(this.resourceEndpoint, {
+            params: {
+                'owner.id': clientId
+            }
+        });
+    }
+
+    getByClientDni(dni) {
+        return http.get(this.resourceEndpoint, {
+            params: {
+                'owner.dni': dni
+            }
+        });
+    }
+    create(data) {
+        return http.post(this.resourceEndpoint, data);
+    }
+}
