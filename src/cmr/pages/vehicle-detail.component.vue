20af49c687511e1cc9b7ecc32176757a48ac000a
elmer-riva <elmeraugustoriva@gmail.com>
2024-10-19 04:09:52 -0500

    feat(intervention-log-timeline): add vehicle detail view with activity log section

--- src/cmr/pages/vehicle-detail.component.vue
+++ src/cmr/pages/vehicle-detail.component.vue
@@ -1,11 +1,117 @@
 <script setup>
+import {ref, onMounted} from 'vue';
+import {useRoute} from 'vue-router';
+import {Vehicle} from "../../service-management/model/vehicle.entity.js";
 
+import {VehicleService} from "../services/vehicle.service.js";
+import PreviousInterventions from "../components/previous-interventions.component.vue";
+
+const route = useRoute();
+const vehicleService = new VehicleService();
+
+const vehicleId = ref(0);
+const vehicle = ref({});
+const currentView = ref('activityLog');
+
+function loadVehicleIdFromRoute() {
+  vehicleId.value = route.params.carId || 0;
+}
+
+function loadVehicleData() {
+  vehicleService.getById(vehicleId.value).then(fetchedVehicle => {
+    vehicle.value = new Vehicle(fetchedVehicle.data);
+    showActivityLog();
+  });
+}
+
+
+function showActivityLog() {
+  currentView.value = 'activityLog';
+}
+
+function showIotInformation() {
+  currentView.value = 'iotInformation';
+}
+
+onMounted(() => {
+  loadVehicleIdFromRoute();
+  loadVehicleData();
+});
 </script>
 
 <template>
-  <h1>Vehicle particular</h1>
+  <main>
+    <div class="vehicle-header">
+      <h1>{{ vehicle.brand }} {{ vehicle.model }} ({{ vehicle.licensePlate }})</h1>
+      <pv-toolbar class="toolbar">
+        <template #start>
+          <pv-button label="Activity log" class="p-button" @click="showActivityLog"/>
+          <pv-button label="IoT Information" class="p-button" @click="showIotInformation"/>
+        </template>
+        <template #end>
+          <pv-button icon="pi pi-trash" class="p-button-danger"/>
+        </template>
+      </pv-toolbar>
+    </div>
+    <div class="vehicle-details">
+      <div class="main-content">
+        <div class="information-container">
+          <previous-interventions
+              v-if="currentView === 'activityLog'"
+              :vehicle="vehicle"
+              :isPanelActive="true"
+          />
+        </div>
+      </div>
+    </div>
+  </main>
 </template>
 
 <style scoped>
+.vehicle-header {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+  width: 100%;
+  box-sizing: border-box;
+}
 
+.toolbar {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-top: 20px;
+}
+
+.p-button {
+  margin-right: 10px;
+}
+
+.vehicle-header h1 {
+  margin: 0;
+  font-size: 2rem;
+}
+
+.vehicle-details {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px;
+  box-sizing: border-box;
+  position: relative;
+}
+
+.main-content {
+  width: 100%;
+  border: 2px solid #ccc;
+  padding: 0 20px;
+  box-sizing: border-box;
+  border-radius: 15px;
+  position: relative;
+}
+
+.information-container {
+  margin-top: 20px;
+}
 </style>
