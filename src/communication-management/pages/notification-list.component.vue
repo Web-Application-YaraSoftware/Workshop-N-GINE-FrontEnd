0c7a9cebedee5a8b5948c4caf84e4b6a5a4652dc
Eric <erich56000@gmail.com>
2024-10-25 21:23:57 -0500

    refactor(notifications-page): use NotificationManager for managing notifications
    - Integrated NotificationManager to handle notifications state.
    - Updated notification fetching to populate notifications in the NotificationManager.
    - Adjusted template to iterate over notifications from NotificationManager.

--- src/communication-management/pages/notification-list.component.vue
+++ src/communication-management/pages/notification-list.component.vue
@@ -3,17 +3,17 @@ import NotificationItem from '../components/notification-item.component.vue'
 import { NotificationService } from "../services/notification.service.js";
 import { Notification } from "../model/notification.entity.js";
 import { onMounted, ref } from "vue";
-import {useWorkshopStore} from "../../shared/services/workshop-store.js";
+import {NotificationManager} from "../model/notification-manager.entity.js";
 
-const notifications = ref([]);
+const notifications = ref();
 const notificationService = new NotificationService();
-const workshopStore = useWorkshopStore();
+const notificationManager = new NotificationManager();
 
 function getNotifications() {
-  notificationService.getByUserId(workshopStore.user.id)
+  notificationService.getByUserId(2)
       .then(response => {
-        notifications.value = buildNotificationListFromResponseData(response.data)
-        console.log(notifications.value)
+        notificationManager.notifications.value = buildNotificationListFromResponseData(response.data)
+        console.log(notificationManager.notifications)
       })
 
 }
@@ -32,7 +32,7 @@ onMounted(() => {
   <section class="notifications-container">
     <h1 class="text-4xl">Notifications</h1>
     <div class="flex flex-column gap-5">
-      <notification-item v-for="notification in notifications" :key="notification.id" :notification="notification" />
+      <notification-item v-for="notification in notificationManager.notifications.value" :key="notification.id" :notification="notification" />
     </div>
   </section>
 </template>
