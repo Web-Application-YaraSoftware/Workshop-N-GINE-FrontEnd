b536e98d27a3ebd047e772411f6bcfe2571c8987
Eric <erich56000@gmail.com>
2024-10-25 23:48:51 -0500

    chore: delete obsolete services and entities for inventory

--- src/service-management/model/inventory-item.entity.js
+++
@@ -1,8 +0,0 @@
-export class InventoryItem {
-    constructor({id = 0, workshop_id = 0, name = '', amount = 0}) {
-        this.id = id;
-        this.workshopId = workshop_id;
-        this.name = name;
-        this.amount = amount;
-    }
-}
