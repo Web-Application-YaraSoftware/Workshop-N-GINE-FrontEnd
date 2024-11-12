c660274f37042d63ee7e5ecaa310120f98776e4e
elmer-riva <elmeraugustoriva@gmail.com>
2024-11-12 13:13:47 -0500

    feat(workshop-service): add methods to get mechanics user id by workshop id, and post mechanic to and specific workshop.

--- src/service-management/services/workshop.service.js
+++ src/service-management/services/workshop.service.js
@@ -1,9 +1,16 @@
 import http from "../../shared/services/http-common.js";
 
 export class WorkshopService {
-    resourceEndpoint = '/workshops';
+    resourceEndPoint = "/workshops";
 
-    getById(id){
-        return http.get(`${this.resourceEndpoint}/${id}`);
+    getById(id) {
+        return http.get(`${this.resourceEndPoint}/${id}`);
+    }
+    getMechanicsUserIdByWorkshopId(workshopId) {
+        return http.get(`${this.resourceEndPoint}/${workshopId}/mechanics`);
+    }
+
+    postMechanicToWorkshop(workshopId, mechanicData) {
+        return http.post(`${this.resourceEndPoint}/${workshopId}/mechanics`, mechanicData);
     }
 }
