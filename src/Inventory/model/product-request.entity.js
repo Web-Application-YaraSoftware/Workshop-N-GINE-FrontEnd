04dac6ed6b8c1acfc24cf7b5732158ef343b61ae
Eric <erich56000@gmail.com>
2024-11-14 01:07:34 -0500

    refactor(product-request): fix attributes and names to match the backend

--- src/Inventory/model/product-request.entity.js
+++ src/Inventory/model/product-request.entity.js
@@ -3,21 +3,17 @@ import { ProductRequestState } from "./product-request-state.enum.js";
 export class ProductRequest {
     constructor({
                     id = 0,
-                    name = '',
                     requestedQuantity = 0,
+                    productId = 0,
                     workshopId = 0,
                     taskId = 0,
-                    requestedDate = '',
-                    observation = '',
                     status = ProductRequestState.PENDING
                 } = {}) {
         this.id = id;
-        this.name = name;
         this.requestedQuantity = requestedQuantity;
+        this.productId = productId;
         this.workshopId = workshopId;
         this.taskId = taskId;
-        this.requestedDate = requestedDate ? new Date(requestedDate) : new Date();
-        this.observation = observation;
         this.status = status;
     }
 }
