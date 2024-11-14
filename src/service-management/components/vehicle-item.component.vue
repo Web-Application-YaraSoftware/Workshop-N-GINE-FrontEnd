959b10531e5dd93e4144721f493d123e9daadc6a
elmer-riva <elmeraugustoriva@gmail.com>
2024-11-14 17:19:54 -0500

    style: workshop n-gine design system applied

--- src/service-management/components/vehicle-item.component.vue
+++ src/service-management/components/vehicle-item.component.vue
@@ -1,11 +1,16 @@
 <script setup>
-import {ref, defineProps, defineEmits} from 'vue';
-import {Vehicle} from "../model/vehicle.entity.js";
+import { ref, defineProps, defineEmits, onMounted, watch } from 'vue';
+import { Vehicle } from "../model/vehicle.entity.js";
+import { InterventionsService } from "../services/interventions.service.js";
+import { Intervention } from "../model/intervention.entity.js";
 
 const props = defineProps({
   vehicle: Vehicle,
   index: Number
 });
+
+const interventionsService = new InterventionsService();
+const registerInterventions = ref([]);
 
 const emits = defineEmits(['selectedVehicle']);
 
@@ -13,11 +18,37 @@ function selectVehicle() {
   emits('selectedVehicle', props.vehicle);
 }
 
-const lastIntervention = ref(
-    props.vehicle?.interventionRegister && props.vehicle?.interventionRegister.length > 0
-        ? props.vehicle?.interventionRegister[props.vehicle?.interventionRegister.length - 1].completionDate
-        : null
-);
+function getRegisterInterventions() {
+  interventionsService.getAllByVehicleId(props.vehicle.id)
+    .then(
+      (response) => {
+        registerInterventions.value = buildInterventionFromResponseData(response.data);
+      },
+      (error) => {
+        console.error(error);
+      }
+    );
+}
+
+function buildInterventionFromResponseData(data) {
+  return data.map((intervention) => new Intervention(intervention));
+}
+
+const lastIntervention = ref(null);
+
+function setLastIntervention() {
+  if (registerInterventions.value.length > 0) {
+    lastIntervention.value = registerInterventions.value.reduce((latest, intervention) => {
+      return new Date(intervention.completionDate) > new Date(latest.completionDate) ? intervention : latest;
+    }).completionDate;
+  }
+}
+
+watch(registerInterventions, setLastIntervention);
+
+onMounted(() => {
+  getRegisterInterventions();
+});
 </script>
 
 <template>
@@ -25,9 +56,9 @@ const lastIntervention = ref(
     <template #header>
       <div class="image-container">
         <img
-            :src="vehicle.image || '../../../public/assets/images/default-car.png'"
-            alt="Vehicle Image"
-            class="vehicle-image"
+          :src="vehicle.image || '../../../public/assets/images/default-car.png'"
+          alt="Vehicle Image"
+          class="vehicle-image"
         />
       </div>
     </template>
@@ -53,8 +84,18 @@ const lastIntervention = ref(
 }
 
 .vehicle-card {
-  border: 1px solid #e0e0e0;
+  border: 1px solid #A0CFFF;
+  background-color: #F3F9FF;
+  border-radius: 12px;
   margin-top: 2rem;
+  padding: 1.5rem;
+  box-shadow: 0 4px 8px rgba(0, 75, 134, 0.1);
+  transition: transform 0.3s ease, box-shadow 0.3s ease;
+}
+
+.vehicle-card:hover {
+  transform: scale(1.03);
+  box-shadow: 0 6px 12px rgba(0, 75, 134, 0.15);
 }
 
 .card-content {
@@ -69,5 +110,4 @@ const lastIntervention = ref(
   font-size: 1.3rem;
   font-weight: 400;
 }
-
 </style>
