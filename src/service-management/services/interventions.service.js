171bc3c20842507db0d38fd14d3eddeec97597ba
Eric <erich56000@gmail.com>
2024-11-16 04:55:36 -0500

    refactor(services): update intervention, product-request, and vehicles services

--- src/service-management/services/interventions.service.js
+++ src/service-management/services/interventions.service.js
@@ -1,43 +1,80 @@
 import http from "../../shared/services/http-common.js";
 
 export class InterventionsService {
+    resourceEndPoint = "/interventions";
+
     getAll() {
-        return http.get('/interventions');
+        return http.get(`${this.resourceEndPoint}`);
     }
 
     getAllByMechanicLeaderId(id) {
-        return http.get(`/interventions?mechanicLeaderId=${id}`);
-    }
-
-    getAllCheckpointsByInterventionIdAndTaskId(interventionId, taskId) {
-        return http.get(`/interventions/${interventionId}/tasks/${taskId}/checkpoints`);
-    }
-
-    getAllTasksByInterventionId(id) {
-        return http.get(`/interventions/${id}/tasks`);
+        return http.get(`${this.resourceEndPoint}?mechanicLeaderId=${id}`);
     }
 
     getAllByMechanicAssistantId(id) {
-        return http.get(`/interventions`);
+        return http.get(`${this.resourceEndPoint}`);
     }
 
     getAllByVehicleId(id) {
-        return http.get(`/interventions?vehicleId=${id}`);
+        return http.get(`${this.resourceEndPoint}?vehicleId=${id}`);
     }
 
     getById(id) {
-        return http.get(`/interventions/${id}`);
+        return http.get(`${this.resourceEndPoint}/${id}`);
     }
 
     post(data) {
-        return http.post('/interventions', data);
+        return http.post(`${this.resourceEndPoint}`, data);
     }
 
     delete(id) {
-        return http.delete(`/interventions/${id}`);
+        return http.delete(`${this.resourceEndPoint}/${id}`);
     }
 
     put(id, data) {
-        return http.put(`/interventions/${id}`, data);
+        return http.put(`${this.resourceEndPoint}/${id}`, data);
     }
+
+    //task related endpoints
+    getAllTasksByInterventionId(interventionId) {
+        return http.get(`${this.resourceEndPoint}/${interventionId}/tasks`);
+    }
+    //TODO: assistant es mechanic leader nomas
+    getAllByMechanicId(id, interventionId) {
+        return http.get(`${this.resourceEndPoint}/${interventionId}/tasks?assistantId=${id}`);
+    }
+    getAllTasksByMechanicIdAndInterventionId(leaderId, interventionId) {
+        return http.get(`${this.resourceEndPoint}/${interventionId}/tasks?mechanicLeaderId=${leaderId}`);
+    }
+
+    postTask(data, interventionId) {
+        return http.post(`${this.resourceEndPoint}/${interventionId}/tasks`, data);
+    }
+
+    deleteTask(id, interventionId) {
+        return http.delete(`${this.resourceEndPoint}/${interventionId}/tasks/${id}`);
+    }
+
+    putTask(id, data, interventionId) {
+        return http.put(`${this.resourceEndPoint}/${interventionId}/tasks/${id}`, data);
+    }
+
+    //checkpoint related endpoints
+    getAllCheckpointsByInterventionIdAndTaskId(taskId, interventionId) {
+        return http.get(`${this.resourceEndPoint}/${interventionId}/tasks/${taskId}/checkpoints`);
+    }
+
+    postCheckpoint(data, taskId, interventionId) {
+        return http.post(`${this.resourceEndPoint}/${interventionId}/tasks/${taskId}/checkpoints`, data);
+    }
+
+    deleteCheckpoint(checkpointId, taskId, interventionId) {
+        return http.delete(`${this.resourceEndPoint}/${interventionId}/tasks/${taskId}/checkpoints/${checkpointId}`);
+    }
+
+    putCheckpoint(checkpointId, taskId, interventionId, data) {
+        return http.put(`${this.resourceEndPoint}/${interventionId}/tasks/${taskId}/checkpoints/${checkpointId}`, data);
+    }
+
+
 }
