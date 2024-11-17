0bed9d5e84442daf83e658003828d3701b06b36d
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2024-11-17 09:39:30 -0500

    refactor(product-requests): simplify product request form and enhance UI - Refactored product request data model to align with new backend requirements, updating property names from 'productType' to 'productId' and 'quantity' to 'requestedQuantity'. - Enhanced user interface with improved feedback by adding product descriptions and a severity-based badge for request status.

--- src/service-management/components/execution-requests.component.vue
+++ src/service-management/components/execution-requests.component.vue
@@ -1,6 +1,5 @@
 <script setup>
 import {computed, onMounted, ref} from "vue";
-//productType es un productId
 const props = defineProps({
   productsStock: {
     type: Array,
@@ -16,6 +15,7 @@ const props = defineProps({
 const emit = defineEmits([
     'add:productRequest', 'update:productRequest'
 ]);
+
 const productRequestsWithProductName = computed(() => {
   return props.productsRequests.map(productsRequest => {
     const product = props.productsStock.find(product => product.id === productsRequest.productId);
@@ -26,20 +26,21 @@ const productRequestsWithProductName = computed(() => {
   });
 });
 const requestedProduct = ref({
-  productType: 0,
-  quantity: 0,
-  description: ""
+  productId: 0,
+  requestedQuantity: 1
 });
-const isRequestProduct = computed(() => typeof requestedProduct.value.productType == 'number' && requestedProduct.value.productType !== 0);
+const isRequestProduct = computed(() => typeof requestedProduct.value.productId == 'number' && requestedProduct.value.productId !== 0);
 const availableAmount = computed(() => {
-  console.log(requestedProduct.value.productType)
-  const product = props.productsStock.find(product => product.id === requestedProduct.value.productType);
+  const product = props.productsStock.find(product => product.id === requestedProduct.value.productId);
   return product ? product.stockQuantity : 0;
 });
+const description = computed(()=>{
+  const product = props.productsStock.find(product => product.id === requestedProduct.value.productId);
+  return product ? product.description : "";
+})
 const loading = ref(false);
-const options = ref(['From stock', 'Requested']);
+const options = ref(['Requested parts']);
 const selectedOption = ref();
-const editingRowsStock = ref([]);
 const editingRowsRequests = ref([]);
 
 onMounted(() => {
@@ -61,9 +62,8 @@ function requestAddProductRequest(){
 
 function resetForm(){
   requestedProduct.value = {
-    productType: 0,
-    quantity: 0,
-    description: ""
+    productId: 0,
+    requestedQuantity: 1
   };
 }
 
@@ -78,6 +78,18 @@ function handleOptionChange(option) {
   selectedOption.value = option;
 }
 
+function getSeverity(status){
+  switch (status) {
+    case 'Pending':
+      return 'warning';
+    case 'Accepted':
+      return 'success';
+    case 'Rejected':
+      return 'danger';
+    default:
+      return 'info';
+  }
+}
 
 </script>
 
@@ -89,7 +101,7 @@ function handleOptionChange(option) {
           <i class="pi pi-cog"></i>
         </pv-inputgroupaddon>
         <pv-select
-            v-model="requestedProduct.productType"
+            v-model="requestedProduct.productId"
             editable
             :options="productsStock"
             optionValue="id"
@@ -105,11 +117,17 @@ function handleOptionChange(option) {
         </pv-inputgroupaddon>
         <pv-inputtext disabled :value="'Total available: '+ availableAmount"/>
       </pv-inputgroup>
+      <pv-inputgroup  v-if="isRequestProduct">
+        <pv-inputgroupaddon>
+          <i class="pi pi-bars"></i>
+        </pv-inputgroupaddon>
+        <pv-inputtext disabled :value="description"/>
+      </pv-inputgroup>
       <pv-inputgroup>
         <pv-inputgroupaddon>
           <i class="pi pi-bars"></i>
         </pv-inputgroupaddon>
-        <pv-inputtext type="number" min="1" v-model="requestedProduct.quantity"/>
+        <pv-inputnumber type="number" :min="1" v-model="requestedProduct.requestedQuantity"/>
       </pv-inputgroup>
 
       <pv-button class="add-button" type="submit" label="Add" icon="pi pi-plus" :loading="loading"/>
@@ -119,57 +137,32 @@ function handleOptionChange(option) {
         <pv-tab v-for="option in options" :key="option" :value="option" @click="handleOptionChange(option)">{{option}}</pv-tab>
       </pv-tablist>
     </pv-tabs>
-    <div v-if="selectedOption === options[0]" class="table">
-      <pv-datatable
-          v-model:editingRows="editingRowsStock"
-          :value="productsStock"
-          editMode="row"
-          dataKey="id"
-          scrollable
-          paginator
-          :rows="5"
-          :rowsPerPageOptions="[5, 10, 15]"
-      >
-        <pv-column field="name" header="Part" style="width: 20rem">
-          <template #body="{data}">{{data.name}}</template>
-          <template #editor="{data, field}">
-            <pv-select v-model="data.id" :options="productsStock" optionLabel="name" optionValue="id" placeholder="Select a product stock" fluid>
-              <template #option="{option}">
-                <div>{{option.name}}</div>
-              </template>
-            </pv-select>
-          </template>
-        </pv-column>
-        <pv-column field="stockQuantity" header="Quantity" style="width: 3rem">
-          <template #editor="{data, field}">
-            <pv-inputtext type="number" min="1" v-model="data[field]"/>
-          </template>
-        </pv-column>
-      </pv-datatable>
-    </div>
-    <div v-if="selectedOption === options[1]" class="table">
-      <pv-datatable
-          v-model:editingRows="editingRowsRequests"
-          :value="productRequestsWithProductName"
-          editMode="row"
-          dataKey="id"
-          scrollable
-          @row-edit-save="onRowEditSaveRequest"
-          paginator
-          :rows="5"
-          :rowsPerPageOptions="[5, 10, 15]"
-      >
-        <pv-column field="productName" header="Part" style="width: 20rem">
-          <template #body="{data}">{{data.productName}}</template>
-        </pv-column>
-        <pv-column field="requestedQuantity" header="Quantity" style="width: 3rem">
-          <template #editor="{data, field}">
-            <pv-inputtext type="number" min="1" v-model="data[field]"/>
-          </template>
-        </pv-column>
-        <pv-column style="width: 2.5rem" :rowEditor="true"/>
-      </pv-datatable>
-    </div>
+    <pv-datatable
+        v-model:editingRows="editingRowsRequests"
+        :value="productRequestsWithProductName"
+        editMode="row"
+        dataKey="id"
+        scrollable
+        @row-edit-save="onRowEditSaveRequest"
+        paginator
+        :rows="5"
+        :rowsPerPageOptions="[5, 10, 15]"
+    >
+      <pv-column field="productName" header="Part" style="width: 20rem">
+        <template #body="data">{{data.data.productName}}</template>
+      </pv-column>
+      <pv-column field="requestedQuantity" header="Quantity" style="width: 3rem">
+        <template #editor="{data, field}">
+          <pv-inputtext type="number" min="1" v-model="data[field]"/>
+        </template>
+      </pv-column>
+      <pv-column field="status" header="Status" style="width: 3rem">
+        <template #body="data">
+          <pv-badge :severity="getSeverity(data.data.status)" :value="data.data.status"/>
+        </template>
+      </pv-column>
+      <pv-column style="width: 2.5rem" :rowEditor="true"/>
+    </pv-datatable>
   </div>
 
 </template>
